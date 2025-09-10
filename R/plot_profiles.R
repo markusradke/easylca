d1d667ce2bfb4ebbebce6f0dfbb7026d7c0a4b02
mradke <mail@markus-radke.de>
2025-09-10 10:58:25 +0200

    change order of plot_function ins plot_profiles according to model_report

--- R/plot_profiles.R
+++ R/plot_profiles.R
@@ -31,6 +31,70 @@ plot_class_prevalences <- function(model){
                    panel.grid = ggplot2::element_blank())
 }
 
+plot_kruskal_profiles <- function(model){
+
+  .get_kruskal_chisquare_from_prediction_for_item <- function(item){
+    are_all_in_same_class <- predicted[['CLASS']] %>% unique() %>% length() == 1
+    if(are_all_in_same_class){ return(0) }
+    stats::kruskal.test(predicted[[toupper(item)]], predicted[['CLASS']])$statistic[[1]] %>%
+      round(2)
+  }
+
+  .get_kruskal_p_from_prediction_for_item <- function(item){
+    are_all_in_same_class <- predicted[['CLASS']] %>% unique() %>% length() == 1
+    if(are_all_in_same_class){ return('') }
+    pvalue <- stats::kruskal.test(predicted[[toupper(item)]], predicted[['CLASS']])$p.value
+    pvalue <- pvalue * nitems # Bonferroni
+    if(pvalue > 0.05){return('')}
+    if(pvalue > 0.05){return('*')}
+    if(pvalue > 0.01){return('**')}
+    return('***')
+  }
+
+  .get_kruskal_chisquares <- function(){
+    data.frame(item = items) %>%
+      dplyr::filter(!stringr::str_detect(.data$item, '#1')) %>%
+      dplyr::mutate(
+        item = as.character(.data$item),
+        chisquare = purrr::map_dbl(.data$item,
+                                   .get_kruskal_chisquare_from_prediction_for_item),
+        p = purrr::map_chr(.data$item,
+                           .get_kruskal_p_from_prediction_for_item)
+      ) %>%
+      dplyr::arrange(-.data$chisquare)
+  }
+
+  predicted <- model$savedata
+  if(is.null(predicted[['CLASS']])) {
+    warning(paste0('CLASS PREDICTIONS FOR ',
+                   model$summaries$Title
+                   ,' WERE NOT STORED BY MPLUS. Check Model outputs for any further hints.'))
+    predicted[['CLASS']] <- 1
+  }
+  items <- model$input$variable$usevariables %>% stringr::str_split_1(' ')
+  items  <- items[items != '']
+  nitems <- items %>% length
+  chisquares <- .get_kruskal_chisquares()
+  max_chi_square <- chisquares$chisquare %>% max
+
+  total_entropy <- model$summaries$Entropy
+  suppressWarnings(
+    ggplot2::ggplot(chisquares, ggplot2::aes(y = stats::reorder(.data$item, .data$chisquare),
+                                             x = .data$chisquare)) +
+      ggplot2::geom_bar(stat = 'identity', fill = 'grey') +
+      ggplot2::geom_text(ggplot2::aes(label = .data$p), hjust = -0.1, size = 6, color = 'black') +
+      ggplot2::scale_x_continuous(limits = c(0, max_chi_square * 1.5), expand = c(0, 0.05),
+                                  position = 'top') +
+      ggplot2::labs(subtitle = 'Kruskal-Wallis Chi square', y = '',
+                    x = '* p < 0.05, **  p < 0.01, *** p < 0.001, Bonferroni corrected')) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(axis.title = ggplot2::element_text(color = 'grey45', size = 10, hjust = 0),
+                   panel.grid = ggplot2::element_blank(),
+                   plot.subtitle = ggplot2::element_text(hjust = 0, size = 12),
+                   axis.text.y = ggplot2::element_text(size = 14, face = 'bold'),
+                   axis.text.x = ggplot2::element_text(size = 10, color = 'grey45', hjust = 0))
+}
+
 plot_continuous_profiles <- function(profiles, ncol_plot=2){
   profiles <- profiles %>% dplyr::filter(.data$plotgroup == 'continuous')
   nclasses <- profiles$class %>% unique() %>% length()
@@ -123,70 +187,6 @@ plot_binary_profiles <- function(profiles){
         panel.grid = ggplot2::element_blank()
       )
     )
-}
-
-plot_kruskal_profiles <- function(model){
-
-  .get_kruskal_chisquare_from_prediction_for_item <- function(item){
-    are_all_in_same_class <- predicted[['CLASS']] %>% unique() %>% length() == 1
-    if(are_all_in_same_class){ return(0) }
-    stats::kruskal.test(predicted[[toupper(item)]], predicted[['CLASS']])$statistic[[1]] %>%
-      round(2)
-  }
-
-  .get_kruskal_p_from_prediction_for_item <- function(item){
-    are_all_in_same_class <- predicted[['CLASS']] %>% unique() %>% length() == 1
-    if(are_all_in_same_class){ return('') }
-    pvalue <- stats::kruskal.test(predicted[[toupper(item)]], predicted[['CLASS']])$p.value
-    pvalue <- pvalue * nitems # Bonferroni
-    if(pvalue > 0.05){return('')}
-    if(pvalue > 0.05){return('*')}
-    if(pvalue > 0.01){return('**')}
-    return('***')
-  }
-
-  .get_kruskal_chisquares <- function(){
-    data.frame(item = items) %>%
-      dplyr::filter(!stringr::str_detect(.data$item, '#1')) %>%
-      dplyr::mutate(
-        item = as.character(.data$item),
-        chisquare = purrr::map_dbl(.data$item,
-                                  .get_kruskal_chisquare_from_prediction_for_item),
-        p = purrr::map_chr(.data$item,
-                          .get_kruskal_p_from_prediction_for_item)
-       ) %>%
-      dplyr::arrange(-.data$chisquare)
-  }
-
-  predicted <- model$savedata
-  if(is.null(predicted[['CLASS']])) {
-    warning(paste0('CLASS PREDICTIONS FOR ',
-                   model$summaries$Title
-                   ,' WERE NOT STORED BY MPLUS. Check Model outputs for any further hints.'))
-    predicted[['CLASS']] <- 1
-  }
-  items <- model$input$variable$usevariables %>% stringr::str_split_1(' ')
-  items  <- items[items != '']
-  nitems <- items %>% length
-  chisquares <- .get_kruskal_chisquares()
-  max_chi_square <- chisquares$chisquare %>% max
-
-  total_entropy <- model$summaries$Entropy
-  suppressWarnings(
-    ggplot2::ggplot(chisquares, ggplot2::aes(y = stats::reorder(.data$item, .data$chisquare),
-                                             x = .data$chisquare)) +
-      ggplot2::geom_bar(stat = 'identity', fill = 'grey') +
-      ggplot2::geom_text(ggplot2::aes(label = .data$p), hjust = -0.1, size = 6, color = 'black') +
-      ggplot2::scale_x_continuous(limits = c(0, max_chi_square * 1.5), expand = c(0, 0.05),
-                                  position = 'top') +
-      ggplot2::labs(subtitle = 'Kruskal-Wallis Chi square', y = '',
-        x = '* p < 0.05, **  p < 0.01, *** p < 0.001, Bonferroni corrected')) +
-    ggplot2::theme_minimal() +
-    ggplot2::theme(axis.title = ggplot2::element_text(color = 'grey45', size = 10, hjust = 0),
-                   panel.grid = ggplot2::element_blank(),
-                   plot.subtitle = ggplot2::element_text(hjust = 0, size = 12),
-                   axis.text.y = ggplot2::element_text(size = 14, face = 'bold'),
-                   axis.text.x = ggplot2::element_text(size = 10, color = 'grey45', hjust = 0))
 }
 
 plot_discrete_profiles <- function(profiles){
